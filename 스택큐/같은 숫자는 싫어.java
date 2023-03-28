48a522a2a9e822864215c5c3e6487a65ad1ce430
jeongmin521 <71782230+jeongmin521@users.noreply.github.com>
2023-03-28 15:37:49 +0900

    주석추가

--- 스택큐/같은 숫자는 싫어.java
+++ 스택큐/같은 숫자는 싫어.java
@@ -5,13 +5,14 @@ public class Solution {
         Stack<Integer> stack = new Stack<>();
         
         for (int i = 0; i < arr.length; i++){
-            if(stack.size() == 0 || arr[i] != stack.peek()) stack.push(arr[i]);
+            if(stack.size() == 0 || arr[i] != stack.peek()) // peek : Stack의 맨 위에 저장된 객체를 반환 Stack에서 꺼내지는 않음. 비었을 때 null 반환
+                stack.push(arr[i]); //스택에 마지막으로 들어간 숫자가 이전에 들어간 숫자와 같지 않으면 arr에 쌓음
         }
         
         int[] answer = new int[stack.size()];
         
         for(int i = stack.size()-1; i >= 0; i--){
-            answer[i] = stack.pop();
+            answer[i] = stack.pop(); //스택은 LIFO라서 뒤에서부터 다시 쌓음
         }
         return answer;
     }
