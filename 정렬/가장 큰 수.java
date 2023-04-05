ce0bf60aa43661f64181a1228d2727b481293525
최정민 <71782230+jeongmin521@users.noreply.github.com>
2023-04-05 22:14:26 +0900

    Create 가장 큰 수.java

---
+++ 정렬/가장 큰 수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] result = new String[numbers.length];
+        for(int i=0; i<numbers.length; i++){
+            result[i]=String.valueOf(numbers[i]);
+        }
+        Arrays.sort(result,(s1,s2)->(s2+s1).compareTo(s1+s2));
+        if(result[0].equals("0")) return "0";
+        for(String s : result){
+            answer+=s;
+        }
+
+        return answer;
+    }
+}
