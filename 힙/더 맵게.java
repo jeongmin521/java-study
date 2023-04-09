59a663311606a65286e98ba8e0704af34c88a72e
최정민 <71782230+jeongmin521@users.noreply.github.com>
2023-04-09 22:55:41 +0900

    주석추가

--- 힙/더 맵게.java
+++ 힙/더 맵게.java
@@ -2,15 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-         PriorityQueue<Integer> pq = new PriorityQueue<>();
+         PriorityQueue<Integer> pq = new PriorityQueue<>(); //숫자가 낮은게 우선
         
-        for (int i : scoville) pq.add(i);
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
         
-        int cnt = 0;
-        while (true) {
-            if (pq.peek()>=K) break;
-            if (pq.size() == 1) return -1;
-            pq.add(pq.poll()+(pq.poll()*2));
+        //=for (int i : scoville) pq.add(i);
+        
+        int cnt = 0; //새로운 음식 만드는 횟수
+        
+        while (pq.peek() < K) {
+            if (pq.peek() >= K) break;
+            if (pq.size() == 1) return - 1; //모두 k 이상으로 만들 수 없는 경우
+            pq.add(pq.poll() + (pq.poll() * 2));
             cnt++;
         }
         
