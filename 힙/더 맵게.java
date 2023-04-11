0e12684d8537ccee8b0550d722ddf42fe7b9fa61
최정민 <71782230+jeongmin521@users.noreply.github.com>
2023-04-11 14:03:11 +0900

    우선순위 큐 설명

--- 힙/더 맵게.java
+++ 힙/더 맵게.java
@@ -22,3 +22,28 @@ class Solution {
         return cnt;
     }
 }
+
+
+/*
+우선순위 큐
+- 큐는 FIFO인데 우선순위큐는 우선순위가 높은 순서대로 나감
+- 이진트리 힙으로 구성
+- 데이터 삽입 시 우선순위의 최대,  최소를 구성하여 데이터가 빠지면 중간을 계속해서 채워넣음
+
+-선언
+PriorityQueue<타입> 변수명 = new PriorityQueue<>();
+
+- 삽입
+해당 큐 맨 뒤에 삽입하고 성공 시 true반환
+add() - 큐가 꽉 찬 경우  IllegalStateException
+offer() - 값 추가 실패 시 false
+
+-삭제
+큐 맨 앞에 있는 값 반환 후 삭제
+remove() - 큐가 비어있는 경우 NoSuchElementException
+poll() - 비어있는 경우 null 반환
+
+-값 확인
+큐의 맨 앞에 있는 값 반환
+element() - 큐가 비어있는 경우 NoSuchElementException
+peek() - 비어있는 경우 null 반환
