ba1c11a66ea7270b4f1248f867fd90514131b027
최정민 <71782230+jeongmin521@users.noreply.github.com>
2023-05-06 23:54:22 +0900

    Update 전화번호 목록.java

--- 해시/전화번호 목록.java
+++ 해시/전화번호 목록.java
@@ -37,3 +37,25 @@ class Solution {
 sort안하고 이중for문 썼을 때 오류가 나는 이유?
 순서가 123 12 이면 false가 나와야하는데 앞의것을 기준으로 뒤에것을 비교하기 때문에 true가 나옴
 */
+
+
+-----------------------------------------------------------------------------------------------------------
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        Map<String, Integer> map = new HashMap<>(); //HashMap 선언
+
+        for (int i = 0; i < phone_book.length; i++) 
+            map.put(phone_book[i], i);
+        
+        for (int i = 0; i < phone_book.length; i++)
+            for (int j = 0; j < phone_book[i].length(); j++){
+                if (map.containsKey(phone_book[i].substring(0, j))){ //모든 전화번호의 substring이 HashMap에 존재하는지 확인
+                    return false;
+                }   
+            }
+        return true;
+    }
+}
