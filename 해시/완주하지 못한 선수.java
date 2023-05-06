df5e14a55d6dd4d570e4d0e9c642f3871f497090
최정민 <71782230+jeongmin521@users.noreply.github.com>
2023-05-06 23:52:54 +0900

    Update 완주하지 못한 선수.java

--- 해시/완주하지 못한 선수.java
+++ 해시/완주하지 못한 선수.java
@@ -14,3 +14,52 @@ class Solution {
     }
 }
 
+
+---------------------------------------------------------------------------------------
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for (String player : participant) //참가자
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        
+        for (String player : completion) //완주자
+            map.put(player, map.get(player) - 1);
+
+            Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+            if (entry.getValue() != 0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+        return answer;
+    }
+}
+
+
+/*
+HashMap : Key-Value의 Pair를 관리하는 클래스
+HashMap<String, Integer>
+    -> Key는 String , Value는 Integer
+    
+getOrDefault
+-  찾는 키 존재 시 찾는 키 값을 반환. 없다면 기본 값 반환
+getOrDefault(Object key, V DefaultValue)
+
+ex)
+String [] alphabet = { "A", "B", "C" ,"A"};
+HashMap<String, Integer> map = new HashMap<>();
+for(String key : alphabet){
+    map.put(key, map.getOrDefault(key, 0) + 1);
+}
+
+-> map : {A=2, B=1, C=1}
+
+*/
