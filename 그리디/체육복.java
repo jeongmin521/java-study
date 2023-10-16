ed68bce99b0f899ab68663fe2c418e407c0b9c86
최정민 <71782230+jeongmin521@users.noreply.github.com>
2023-10-16 14:50:47 +0900

    Update 체육복.java
    sort추가

--- 그리디/체육복.java
+++ 그리디/체육복.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-            int answer = n - lost.length;
- 
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        int answer = n - lost.length;
+        
         //본인이 도난당함
         for (int i = 0; i < lost.length; i++) {
             for (int j = 0; j < reserve.length; j++) {
