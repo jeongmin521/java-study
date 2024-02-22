945945dd858dc8b47cda83df6897ab8bcc5f856f
최정민 <71782230+jeongmin521@users.noreply.github.com>
2024-02-22 17:31:17 +0900

    Update 가장 큰 수.java

--- 정렬/가장 큰 수.java
+++ 정렬/가장 큰 수.java
@@ -7,7 +7,12 @@ class Solution {
         for(int i=0; i<numbers.length; i++){
             result[i]=String.valueOf(numbers[i]);
         }
-        Arrays.sort(result,(s1,s2)->(s2+s1).compareTo(s1+s2));
+        //Arrays.sort(result,(s1,s2)->(s2+s1).compareTo(s1+s2)); 람다식
+        Arrays.sort(result, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return (s2 + s1).compareTo(os + s2);
+            }
+        });
         if(result[0].equals("0")) return "0";
         for(String s : result){
             answer+=s;
