8d534960953942c3e23961d64c3ee429470b9aa2
최정민 <71782230+jeongmin521@users.noreply.github.com>
2024-02-26 17:46:44 +0900

    Update 같은 숫자는 싫어.java

--- 스택큐/같은 숫자는 싫어.java
+++ 스택큐/같은 숫자는 싫어.java
@@ -17,3 +17,55 @@ public class Solution {
         return answer;
     }
 }
+
+-------------------------------------------------------------------------------------------------------------------------------
+    import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        List<Integer> answer = new ArrayList<Integer>();
+//         Queue<Integer> q = new LinkedList<>(); //정수형 큐 선언
+        
+//         int qvalue = -1; //arr의 원소가 될 수 없는 값으로 초기값 설정
+        
+//         for(int x : arr){
+//             q.add(x);
+//         } //큐에 arr에 있는 숫자들 넣기
+        
+//         for(int i = 0; i < arr.length; i++){
+//             int qelement = q.element();
+//             if(qvalue == qelement){ //숫자 중복 여부 확인
+//                 q.remove(); //중복 값일 경우 삭제
+//                 continue;
+//             }
+//             else{
+//                 qvalue = q.poll();
+//                 answer.add(qvalue);//answer에 중복되지 않은 숫자 넣기
+//             }
+//         }
+        
+        int qvalue = -1; //arr의 원소가 될 수 없는 값으로 초기값 설정
+        
+        for(int value : arr){
+            if(qvalue == value){ //숫자 중복 여부 확인
+                continue;
+            }
+            else{
+                answer.add(value);//answer에 중복되지 않은 숫자 넣기
+                qvalue = value;
+            }
+        }
+
+        return answer.stream().mapToInt(i -> i.intValue()).toArray();
+    }
+}
+
+/*
+중복된 숫자는 빼고 입력 순서 그대로 다시 출력해야 함 -> FIF0. 큐 사용하기
+큐의 값을 하나씩 빼면서 그 값과 그 다음으로 나올 값을 비교해 같을 경우 삭제
+peek을 하고 성공하면 true/false를 반환함. 형 변환 필요
+ ->peek() 대신 element, poll 사용
+테케 다 통과했는데 효율성검사에서 시간초과가 뜸..
+굳이 큐를 쓰지 않아도 가능
+*/
